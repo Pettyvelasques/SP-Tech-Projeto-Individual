2c45932f2c6d1d99c1baa981f8a6ac71ace3437f
Patrick Velasques <pettyvelasques@hotmail.com>
2023-11-12 19:16:52 -0300

    Finalizada a seção de Garagem, Incluida a tela de Seleção de Garagem ou Loja que será a futura tela de login, Iniciada a seção de Loja

--- Moto-Viagens/script.js
+++ Moto-Viagens/script.js
@@ -1,24 +1,29 @@
-var motosNaGaragem = ["CRF-1100L", "PCX-150", "CB-300F", "CG-160"];
+var motosNaGaragem = ["CRF-1100L", "CG-160"];
 var imagemMotoAtual = document.getElementById("motocicletaAtual");
 var imagemMotoOut = document.getElementById("motocicletaGetIn");
 var imagemMotoIn = document.getElementById("motocicletaGetOut");
 var motoAtual = imagemMotoAtual.classList;
 var indexAtual = 0;
+var executandoAnimacao = 0;
 
 function anteriorMotocicleta() {
+	if (executandoAnimacao == 1) {
+		return false;
+	}
+	executandoAnimacao = 1;
 	if (motoAtual.value != -1) {
 		indexAtual = motosNaGaragem.indexOf(motoAtual.value);
 	}
 	if (indexAtual == 0) {
-		imagemMotoIn.src = `../Banco-de-Motos/Honda/${motosNaGaragem[indexAtual]}.webp`;
+		imagemMotoIn.src = `../Banco-de-Motos/Honda/${motosNaGaragem[indexAtual]}.png`;
 		imagemMotoIn.style = `display: block`;
 		imagemMotoAtual.style = `display: none`;
 
 		indexAtual = motosNaGaragem.length - 1;
-		imagemMotoAtual.src = `../Banco-de-Motos/Honda/${motosNaGaragem[indexAtual]}.webp`;
+		imagemMotoAtual.src = `../Banco-de-Motos/Honda/${motosNaGaragem[indexAtual]}.png`;
 		imagemMotoAtual.classList = `${motosNaGaragem[indexAtual]}`;
 
-		imagemMotoOut.src = `../Banco-de-Motos/Honda/${motosNaGaragem[indexAtual]}.webp`;
+		imagemMotoOut.src = `../Banco-de-Motos/Honda/${motosNaGaragem[indexAtual]}.png`;
 		imagemMotoOut.style = `display: block`;
 
 		imagemMotoIn.classList = `motoLeftOut`;
@@ -31,17 +36,18 @@ function anteriorMotocicleta() {
 
 			imagemMotoIn.classList = ``;
 			imagemMotoOut.classList = ``;
-		}, 1450);
+			executandoAnimacao = 0;
+		}, 950);
 	} else {
-		imagemMotoIn.src = `../Banco-de-Motos/Honda/${motosNaGaragem[indexAtual]}.webp`;
+		imagemMotoIn.src = `../Banco-de-Motos/Honda/${motosNaGaragem[indexAtual]}.png`;
 		imagemMotoIn.style = `display: block`;
 		imagemMotoAtual.style = `display: none`;
 
 		indexAtual -= 1;
-		imagemMotoAtual.src = `../Banco-de-Motos/Honda/${motosNaGaragem[indexAtual]}.webp`;
+		imagemMotoAtual.src = `../Banco-de-Motos/Honda/${motosNaGaragem[indexAtual]}.png`;
 		imagemMotoAtual.classList = `${motosNaGaragem[indexAtual]}`;
 
-		imagemMotoOut.src = `../Banco-de-Motos/Honda/${motosNaGaragem[indexAtual]}.webp`;
+		imagemMotoOut.src = `../Banco-de-Motos/Honda/${motosNaGaragem[indexAtual]}.png`;
 		imagemMotoOut.style = `display: block`;
 
 		imagemMotoIn.classList = `motoLeftOut`;
@@ -54,24 +60,28 @@ function anteriorMotocicleta() {
 
 			imagemMotoIn.classList = ``;
 			imagemMotoOut.classList = ``;
-		}, 1450);
+			executandoAnimacao = 0;
+		}, 950);
 	}
 }
 function proximaMotocicleta() {
-	console.log(indexAtual);
+	if (executandoAnimacao == 1) {
+		return false;
+	}
+	executandoAnimacao = 1;
 	if (motoAtual.value != -1) {
 		indexAtual = motosNaGaragem.indexOf(motoAtual.value);
 	}
 	if (indexAtual == motosNaGaragem.length - 1) {
-		imagemMotoIn.src = `../Banco-de-Motos/Honda/${motosNaGaragem[indexAtual]}.webp`;
+		imagemMotoIn.src = `../Banco-de-Motos/Honda/${motosNaGaragem[indexAtual]}.png`;
 		imagemMotoIn.style = `display: block`;
 		imagemMotoAtual.style = `display: none`;
 
 		indexAtual = 0;
-		imagemMotoAtual.src = `../Banco-de-Motos/Honda/${motosNaGaragem[indexAtual]}.webp`;
+		imagemMotoAtual.src = `../Banco-de-Motos/Honda/${motosNaGaragem[indexAtual]}.png`;
 		imagemMotoAtual.classList = `${motosNaGaragem[indexAtual]}`;
 
-		imagemMotoOut.src = `../Banco-de-Motos/Honda/${motosNaGaragem[indexAtual]}.webp`;
+		imagemMotoOut.src = `../Banco-de-Motos/Honda/${motosNaGaragem[indexAtual]}.png`;
 		imagemMotoOut.style = `margin-right: 0; margin-left:1500px; display: block`;
 
 		imagemMotoIn.classList = `motoRightOut`;
@@ -84,17 +94,18 @@ function proximaMotocicleta() {
 
 			imagemMotoIn.classList = ``;
 			imagemMotoOut.classList = ``;
-		}, 1450);
+			executandoAnimacao = 0;
+		}, 950);
 	} else {
-		imagemMotoIn.src = `../Banco-de-Motos/Honda/${motosNaGaragem[indexAtual]}.webp`;
+		imagemMotoIn.src = `../Banco-de-Motos/Honda/${motosNaGaragem[indexAtual]}.png`;
 		imagemMotoIn.style = `display: block`;
 		imagemMotoAtual.style = `display: none`;
 
 		indexAtual += 1;
-		imagemMotoAtual.src = `../Banco-de-Motos/Honda/${motosNaGaragem[indexAtual]}.webp`;
+		imagemMotoAtual.src = `../Banco-de-Motos/Honda/${motosNaGaragem[indexAtual]}.png`;
 		imagemMotoAtual.classList = `${motosNaGaragem[indexAtual]}`;
 
-		imagemMotoOut.src = `../Banco-de-Motos/Honda/${motosNaGaragem[indexAtual]}.webp`;
+		imagemMotoOut.src = `../Banco-de-Motos/Honda/${motosNaGaragem[indexAtual]}.png`;
 		imagemMotoOut.style = `margin-right: 0; margin-left:1500px; display: block`;
 
 		imagemMotoIn.classList = `motoRightOut`;
@@ -107,6 +118,81 @@ function proximaMotocicleta() {
 
 			imagemMotoIn.classList = ``;
 			imagemMotoOut.classList = ``;
-		}, 1450);
+			executandoAnimacao = 0;
+		}, 950);
 	}
+}
+
+function exibirDetalhes() {
+	var modalDetalhes = document.getElementById("modalDetalhesMoto");
+
+	modalDetalhes.style = "display: flex;";
+	console.log(modalDetalhes.style.display);
+}
+
+function fecharDetalhes() {
+	var modalDetalhes = document.getElementById("modalDetalhesMoto");
+
+	modalDetalhes.style = "display: none;";
+}
+
+function entrarGaragem() {
+	var entrada = document.getElementById("telaEntrada");
+	var garagem = document.getElementById("telaGaragem");
+	var loja = document.getElementById("telaLoja");
+
+	entrada.style = "display: none;";
+	garagem.style = "display: block;";
+	loja.style = "display: none;";
+}
+
+function sairGaragem() {
+	var entrada = document.getElementById("telaEntrada");
+	var garagem = document.getElementById("telaGaragem");
+	var loja = document.getElementById("telaLoja");
+
+	entrada.style = "display: flex;";
+	garagem.style = "display: none;";
+	loja.style = "display: none;";
+}
+
+function entrarLoja() {
+	var entrada = document.getElementById("telaEntrada");
+	var loja = document.getElementById("telaLoja");
+	var garagem = document.getElementById("telaGaragem");
+
+	entrada.style = "display: none;";
+	loja.style = "display: block;";
+	garagem.style = "display: none;";
+}
+
+function sairLoja() {
+	var entrada = document.getElementById("telaEntrada");
+	var loja = document.getElementById("telaLoja");
+	var garagem = document.getElementById("telaGaragem");
+
+	entrada.style = "display: flex;";
+	loja.style = "display: none;";
+	garagem.style = "display: none;";
+}
+
+function marcarMoto() {
+	var checkMoto = document.getElementById("modeloCheck");
+	var botaoCompra = document.getElementById("botaoComprar");
+	var botaoVenda = document.getElementById("botaoVender");
+
+	botaoVenda.style = "display: flex;";
+	botaoCompra.style = "display: none;";
+	checkMoto.src = "../Rascunhos/Banco-de-Imagens/No-BG/buyed-check.png";
+	checkMoto.style = "filter: brightness(90%);";
+}
+function desmarcarMoto() {
+	var checkMoto = document.getElementById("modeloCheck");
+	var botaoCompra = document.getElementById("botaoComprar");
+	var botaoVenda = document.getElementById("botaoVender");
+
+	botaoVenda.style = "display: none;";
+	botaoCompra.style = "display: flex;";
+	checkMoto.src = "../Rascunhos/Banco-de-Imagens/No-BG/buyed.png";
+	checkMoto.style = "filter: opacity(30%);";
 }
